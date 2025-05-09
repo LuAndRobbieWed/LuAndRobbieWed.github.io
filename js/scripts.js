2a99740d7563274599f3dbfd1921edf58e7cd30d
JohnandAshWed <97843668+JohnandAshWed@users.noreply.github.com>
2025-05-09 16:45:40 +0100

    update text and rsvp

--- js/scripts.js
+++ js/scripts.js
@@ -208,7 +208,7 @@ $(document).ready(function () {
 
 
     /********************** RSVP **********************/
-    $('1z5_9kEB-oUUTxyKOew29L6UiERa4apwPAhJh4QnbnwE').on('submit', function (e) {
+    $('rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
 
